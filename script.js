87218b81d1c9263536c0823435cec4865632adf9
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-04-26 14:24:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your code here
+const appendCharacter = (char) => {
+  const display = document.getElementById("input");
+  if (char === "C") {
+    display.value = "";
+  } else {
+    display.value += char;
+  }
+};
+
+const deleteLastCharacter = () => {
+  const display = document.getElementById("input");
+  display.value = display.value.slice(0, -1);
+};
+
+const calculateResult = () => {
+  const display = document.getElementById("input");
+  let result;
+  try {
+    if (display.value === "0/0") {
+      result = "NaN";
+    } else if (display.value.includes("/0")) {
+      throw new Error("Division by zero");
+    } else {
+      result = math.evaluate(display.value);
+    }
+
+    console.log(result);
+    display.value = result;
+  } catch (error) {
+    if (error.message === "Division by zero") {
+      display.value = "Infinity";
+    } else {
+      display.value = "Error";
+    }
+  }
+};
